83892192f6fa4f85fddcd67e13d3ee8e07c0c26e
Dent7m <kafkastell@gmail.com>
2025-11-06 23:19:40 +0400

    Update MainClass.h

--- MainClass.h
+++ MainClass.h
@@ -1,107 +1,37 @@
 #pragma once
 #include <iostream>
 #include <fstream>
-#include "Main.cpp"
 
 using namespace std;
 
 class Npc
 {
-protected: //модификатор 0 защищенный (дает доступ внутри класса родителя и наследника)
-    //но все еще не дает доступ в основном потоке программы
+protected: //РјРѕРґРёС„РёРєР°С‚РѕСЂ 0 Р·Р°С‰РёС‰РµРЅРЅС‹Р№ (РґР°РµС‚ РґРѕСЃС‚СѓРї РІРЅСѓС‚СЂРё РєР»Р°СЃСЃР° СЂРѕРґРёС‚РµР»СЏ Рё РЅР°СЃР»РµРґРЅРёРєР°)
+    //РЅРѕ РІСЃРµ РµС‰Рµ РЅРµ РґР°РµС‚ РґРѕСЃС‚СѓРї РІ РѕСЃРЅРѕРІРЅРѕРј РїРѕС‚РѕРєРµ РїСЂРѕРіСЂР°РјРјС‹
 
-    string name{ "персонаж" };
+    string name{ "РїРµСЂСЃРѕРЅР°Р¶" };
     unsigned int health{ 10 };
     float damage{ 5 };
     unsigned short lvl{ 1 };
 
-public:    //публичный модификатор доступ (использовать метод можно в любом месте)
-    string GetName()
-    {
-        return name;
-    }
-    unsigned int GetHealth()
-    {
-        return health;
-    }
-    float GetDamage()
-    {
-        return damage;
-    }
-    unsigned int GetLvl()
-    {
-        return lvl;
-    }
-    /*unsigned int GetIntellect()
-    {
-        return intellect;
-    }
-    unsigned int GetStrenght()
-    {
-        return strenght;
-    }*/
-    virtual void GetInfo() //метод класса
-    {
-        cout << "Имя - " << name << endl;
-        cout << "Здоровье - " << health << endl;
-        cout << "Урон - " << damage << endl;
-    }
+public:    //РїСѓР±Р»РёС‡РЅС‹Р№ РјРѕРґРёС„РёРєР°С‚РѕСЂ РґРѕСЃС‚СѓРї (РёСЃРїРѕР»СЊР·РѕРІР°С‚СЊ РјРµС‚РѕРґ РјРѕР¶РЅРѕ РІ Р»СЋР±РѕРј РјРµСЃС‚Рµ)
+    string GetName();
+    unsigned int GetHealth();
+    float GetDamage();
+    unsigned int GetLvl();
+    virtual void GetInfo();
 
     virtual void Create() {};
-    virtual bool Save()
-    {
+    virtual bool Save();
 
-        ofstream saveSystem("save.bin", ios::binary);
-        if (saveSystem.is_open())
-        {
-            saveSystem.write(reinterpret_cast<const char*>(&name), sizeof(name));
-            saveSystem.write(reinterpret_cast<const char*>(&health), sizeof(health));
-            saveSystem.write(reinterpret_cast<const char*>(&damage), sizeof(damage));
-            saveSystem.write(reinterpret_cast<const char*>(&lvl), sizeof(lvl));
-            saveSystem.close();
-            return true;
-        }
-        else
-        {
-            cout << "Сохранение не удалось\nПопробуйте позже\n";
-            return false;
-        }
-        saveSystem.close();
-    };
-    Npc Load()
-    {
-        ifstream loadSystem("save.bin", ios::binary);
-        Npc npc; //временное хранилище для считывания данных из файла
-        if (loadSystem.is_open())
-        {
-            loadSystem.read(reinterpret_cast<char*>(&npc.name), sizeof(npc.name));
-            loadSystem.read(reinterpret_cast<char*>(&npc.health), sizeof(npc.health));
-            loadSystem.read(reinterpret_cast<char*>(&npc.damage), sizeof(npc.damage));
-            loadSystem.read(reinterpret_cast<char*>(&npc.lvl), sizeof(npc.lvl));
-        }
-        else
-        {
-            cout << "Связь с небом нарушена\nПамять о ваших прошлых путешествиях повреждена\n";
-            return npc;
-        }
-        loadSystem.close();
-        return npc;
-    };
+    Npc Load();
 
 };
 class Player
 {
 public:
-    void Create(Npc* player)
-    {
-        player->Create();
-    }
-    void Save(Npc* player)
-    {
-        player->Save();
-    }
-    void Load(Npc* player)
-    {
-        player->Load();
-    }
+    void Create(Npc* player);
+    void Save(Npc* player);
+    void Load(Npc* player);
+
 };
